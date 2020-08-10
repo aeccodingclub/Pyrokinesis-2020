968aa789d32a9df430586ab9b7086eab85a298fa
Hrishikesh Deka (Jack) <hridehd007@gmail.com>
2020-08-10 18:24:50 +0530

    Added all files of the website

---
+++ team/js/logics.js
@@ -0,0 +1,10 @@
+function display(a)
+{
+	document.getElementById('details'+String(a)).style.display='block';
+
+}
+function displayoff(a)
+{
+	document.getElementById('details'+String(a)).style.display='none';
+
+}
